9833232096eb39f2611a67d022579807b55750ff
Alex DeGrave <ajd98@pitt.edu>
2018-03-06 08:07:36 -0800

    Warning colors for tests

---
+++ tests/warningcolors.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# warningcolors.py
+
+class terminalcolors(object):
+    RESET = u'\u001b[0m' # reset to default
+    FAIL = u'\u001b[91m' # red
+    WARN = u'\u001b[33m' # yellow
+    OKAY = u'\u001b[32m' # green
+    BLUE = u'\u001b[34m' # blue
