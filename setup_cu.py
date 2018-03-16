d283a6f4b7cd6a5078da379dd4938eb5357b0a51
Alex DeGrave <ajd98@pitt.edu>
2018-03-15 23:00:44 -0400

    script to cythonize cuda wrapper

---
+++ setup_cu.py
@@ -0,0 +1,24 @@
+import numpy
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+import os
+
+libdir = os.path.join(os.path.abspath(os.getcwd()), 'kde', 'evaluate')
+include_dirs = ['./include', './kde/cuda']
+
+ext_modules=[
+    Extension("kde.cuda.evaluate",
+        sources=["kde/cuda/evaluate.pyx"],
+        libraries=["m"],
+        library_dirs=[libdir],
+        include_dirs=include_dirs,
+        extra_compile_args=["-O3"]
+    )
+]
+
+
+setup(
+    name="kde.cuda.evaluate",
+    ext_modules=cythonize(ext_modules),
+    include_dirs=[numpy.get_include()],
+)
