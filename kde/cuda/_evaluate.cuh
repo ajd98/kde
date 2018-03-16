5a55f93b66f3851655342c2e45b189b7c3271741
Alex DeGrave <ajd98@pitt.edu>
2018-03-15 22:46:05 -0400

    header for cuda functions

---
+++ kde/cuda/_evaluate.cuh
@@ -0,0 +1,12 @@
+#ifndef
+#define EVALUATE_CU_H
+void
+_evaluate_cu(const double* query_points, 
+             const double* training_points, 
+             const double* weights,
+             enum metricopt metric_idx,
+             enum kernelopt kernel_idx,
+             double h, 
+             double* result,
+             int nquery, int ntrain, int ndim);
+#endif // EVALUATE_CU_H
