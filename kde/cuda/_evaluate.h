9d3c9a97559b080ecc72ebddcbde3f90b7aff126
Alex DeGrave <ajd98@pitt.edu>
2018-03-16 15:41:45 -0400

    C-style header; needed since nvcc compiles c++-style

---
+++ kde/cuda/_evaluate.h
@@ -0,0 +1,12 @@
+#ifndef EVALUATE_CU_H
+#define EVALUATE_CU_H
+void
+cuda_evaluate(const double* query_points,
+              const double* training_points, 
+              const double* weights,
+              char* metric_s,
+              char* kernel_s,
+              double h, 
+              double* result,
+              int nquery, int ntrain, int ndim);
+#endif // EVALUATE_CU_H
