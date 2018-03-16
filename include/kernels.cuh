695f8169d8adb85f25b78a6d07b290c367f73a74
Alex DeGrave <ajd98@pitt.edu>
2018-03-16 15:43:11 -0400

    specify extern for device functions

--- include/kernels.cuh
+++ include/kernels.cuh
@@ -1,12 +1,12 @@
 #ifndef KERNELS_H
 #define KERNELS_H
-__device__ double bump(double x);
-__device__ double cosine_kernel(double x);
-__device__ double epanechnikov(double x);
-__device__ double gaussian(double x);
-__device__ double logistic(double x);
-__device__ double quartic(double x);
-__device__ double tophat(double x);
-__device__ double triangle(double x);
-__device__ double tricube(double x);
+extern __device__ double bump(double x);
+extern __device__ double cosine_kernel(double x);
+extern __device__ double epanechnikov(double x);
+extern __device__ double gaussian(double x);
+extern __device__ double logistic(double x);
+extern __device__ double quartic(double x);
+extern __device__ double tophat(double x);
+extern __device__ double triangle(double x);
+extern __device__ double tricube(double x);
 #endif // KERNELS_H
