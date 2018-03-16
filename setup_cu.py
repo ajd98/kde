3901548ffbbed15cd3fdbe502fe76044fb173dcc
Alex DeGrave <ajd98@pitt.edu>
2018-03-16 10:58:15 -0400

    Link to libcukde

--- setup_cu.py
+++ setup_cu.py
@@ -3,13 +3,13 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import os
 
-libdir = os.path.join(os.path.abspath(os.getcwd()), 'kde', 'evaluate')
+libdir = os.path.join(os.path.abspath(os.getcwd()), 'kde', 'cuda')
 include_dirs = ['./include', './kde/cuda']
 
 ext_modules=[
     Extension("kde.cuda.evaluate",
         sources=["kde/cuda/evaluate.pyx"],
-        libraries=["m"],
+        libraries=["m", "cukde"],
         library_dirs=[libdir],
         include_dirs=include_dirs,
         extra_compile_args=["-O3"]
