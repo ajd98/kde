7f72de6241a41b32fdd01f229a8660ba13887dc5
Alex DeGrave <ajd98@pitt.edu>
2018-03-19 18:26:10 -0400

    Make cuda backend optional.  Move tests to separate functions.

--- tests/normalization.py
+++ tests/normalization.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import argparse
 import sys
 sys.path.append('../')
 sys.path.append('../kde')
@@ -19,47 +20,58 @@ kernels = ['bump',
            'tricube']
 
 compactkernels = kde.evaluate.COMPACT_KERNELS
-
-def main():
+def test_1d_euclidean(cuda=False):
     # Test kernel normalization in one dimensional euclidean space
     print("Testing kernel normalization in 1D Euclidean space")
     training_points_1d = numpy.zeros(1)[numpy.newaxis,:]
     for kernel in kernels:
         def f(x):
             x = numpy.array((x,))[numpy.newaxis, :]
-            return kde.evaluate.estimate_pdf_brute(x, training_points_1d, kernel=kernel, bandwidth=1)[0]
+            return kde.evaluate.estimate_pdf_brute(x, training_points_1d, 
+                           kernel=kernel, bandwidth=1, cuda=cuda)[0]
         integral = scipy.integrate.quad(f, -20, 20)[0]
     
         if numpy.isclose(integral, 1):
-            print("    Test passed: 1D kernel '{:s}' integrates to {:.02f}".format(kernel, integral))
+            print("    Test passed: 1D kernel '{:s}' integrates to {:.02f}"\
+                  .format(kernel, integral))
         else:
             print(terminalcolors.FAIL, end='')
-            print("    TEST FAILED: 1D kernel '{:s}' integrates to {:.02f}.".format(kernel, integral))
+            print("    TEST FAILED: 1D kernel '{:s}' integrates to {:.02f}."\
+                  .format(kernel, integral))
             print(terminalcolors.RESET, end='')
 
+def test_1d_euclidean_bw2(cuda=False):
     # Test kernel normalization in one dimensional euclidean space
-    print("Testing kernel normalization in 1D Euclidean space, with bandwidth of 2")
+    print("Testing kernel normalization in 1D Euclidean space, with bandwidth "
+          "of 2")
     training_points_1d = numpy.zeros(1)[numpy.newaxis,:]
     for kernel in kernels:
         def f(x):
             x = numpy.array((x,))[numpy.newaxis, :]
-            return kde.evaluate.estimate_pdf_brute(x, training_points_1d, kernel=kernel, bandwidth=2)[0]
+            return kde.evaluate.estimate_pdf_brute(x, training_points_1d, 
+                          kernel=kernel, bandwidth=2, cuda=cuda)[0]
         integral = scipy.integrate.quad(f, -20, 20)[0]
     
         if numpy.isclose(integral, 1, atol=0.001):
-            print("    Test passed: 1D kernel '{:s}' integrates to {:.02f}".format(kernel, integral))
+            print("    Test passed: 1D kernel '{:s}' integrates to {:.02f}"\
+                  .format(kernel, integral))
         else:
             print(terminalcolors.FAIL, end='')
-            print("    TEST FAILED: 1D kernel '{:s}' integrates to {:.02f}.".format(kernel, integral))
+            print("    TEST FAILED: 1D kernel '{:s}' integrates to {:.02f}."\
+                  .format(kernel, integral))
             print(terminalcolors.RESET, end='')
-    
-    # Test kernel normalization in one dimensional euclidean space, with multiple training points
-    print("Testing kernel normalization in 1D Euclidean space, with multiple training points")
+
+def test_1d_euclidean_multiple_training(cuda=False):
+    # Test kernel normalization in one dimensional euclidean space, with 
+    # multiple training points
+    print("Testing kernel normalization in 1D Euclidean space, with multiple "
+          " training points")
     training_points_1d = numpy.zeros(10)[:, numpy.newaxis]
     for kernel in kernels:
         def f(x):
             x = numpy.array((x,))[numpy.newaxis, :]
-            return kde.evaluate.estimate_pdf_brute(x, training_points_1d, kernel=kernel)[0]
+            return kde.evaluate.estimate_pdf_brute(x, training_points_1d, 
+                           kernel=kernel, cuda=cuda)[0]
         integral = scipy.integrate.quad(f, -20, 20)[0]
     
         if numpy.isclose(integral, 1):
@@ -71,23 +83,28 @@ def main():
                   "training points.".format(kernel, integral))
             print(terminalcolors.RESET, end='')
 
+def test_1d_euclidean_ntorus(cuda=False):
     # Test kernel normalization in one dimensional n-Torus space
-    print("Testing kernel normalization in 1D n-Torus space, with multiple training points")
+    print("Testing kernel normalization in 1D n-Torus space, with multiple "
+          "training points")
     training_points_1d = 180*numpy.ones(1)[:, numpy.newaxis]
     for kernel in compactkernels:
         def f(x):
             x = numpy.array((x,))[numpy.newaxis, :]
-            return kde.evaluate.estimate_pdf_brute(x, training_points_1d, kernel=kernel, metric='euclidean_distance_ntorus')[0]
+            return kde.evaluate.estimate_pdf_brute(x, training_points_1d, 
+                kernel=kernel, metric='euclidean_distance_ntorus', cuda=cuda)[0]
         integral = scipy.integrate.quad(f, -180, 180)[0]
     
         if numpy.isclose(integral, 1):
-            print("    Test passed: 1D kernel '{:s}' integrates to {:.02f}".format(kernel, integral))
+            print("    Test passed: 1D kernel '{:s}' integrates to {:.02f}"\
+                  .format(kernel, integral))
         else:
             print(terminalcolors.FAIL, end='')
-            print("    TEST FAILED: 1D kernel '{:s}' integrates to {:.02f}.".format(kernel, integral))
+            print("    TEST FAILED: 1D kernel '{:s}' integrates to {:.02f}."\
+                  .format(kernel, integral))
             print(terminalcolors.RESET, end='')
-    
-    
+
+def test_2d_euclidean(cuda=False):
     # Test kernel normalization in two dimensional euclidean space
     print("Testing kernel normalization in 2D Euclidean space")
     training_points_2d = numpy.zeros(2)[numpy.newaxis,:]
@@ -110,16 +127,20 @@ def main():
     
         def f(x, y):
             x = numpy.array((x,y))[numpy.newaxis, :]
-            return kde.evaluate.estimate_pdf_brute(x, training_points_2d, kernel=kernel)[0]
+            return kde.evaluate.estimate_pdf_brute(x, training_points_2d, 
+                                                   kernel=kernel, cuda=cuda)[0]
     
         integral = scipy.integrate.dblquad(f, xmin, xmax, ymin, ymax, epsabs=.01)[0]
         if numpy.isclose(integral, 1, atol=0.01):
-            print("    Test passed: 2D kernel '{:s}' integrates to {:.02f}".format(kernel, integral))
+            print("    Test passed: 2D kernel '{:s}' integrates to {:.02f}"\
+                  .format(kernel, integral))
         else:
             print(terminalcolors.FAIL, end='')
-            print("    TEST FAILED: 2D kernel '{:s}' integrates to {:.02f}.".format(kernel, integral))
+            print("    TEST FAILED: 2D kernel '{:s}' integrates to {:.02f}."\
+                  .format(kernel, integral))
             print(terminalcolors.RESET, end='')
 
+def test_2d_euclidean_ntorus(cuda=False):
     # Test kernel normalization in two dimensional n-torus space
     print("Testing kernel normalization in 2-torus space")
     training_points_2d = 180*numpy.ones(2)[numpy.newaxis,:]
@@ -139,7 +160,8 @@ def main():
     
         def f(x, y):
             x = numpy.array((x,y))[numpy.newaxis, :]
-            return kde.evaluate.estimate_pdf_brute(x, training_points_2d, kernel=kernel, metric='euclidean_distance_ntorus')[0]
+            return kde.evaluate.estimate_pdf_brute(x, training_points_2d, 
+                kernel=kernel, metric='euclidean_distance_ntorus', cuda=cuda)[0]
     
         integral1 = scipy.integrate.dblquad(f, xmin1, xmax1, ymin1, ymax1, epsabs=.01)[0]
         integral2 = scipy.integrate.dblquad(f, xmin1, xmax1, ymin2, ymax2, epsabs=.01)[0]
@@ -148,12 +170,15 @@ def main():
 
         integral = integral1 + integral2 + integral3 + integral4
         if numpy.isclose(integral, 1, atol=0.01):
-            print("    Test passed: 2D kernel '{:s}' integrates to {:.02f}".format(kernel, integral))
+            print("    Test passed: 2D kernel '{:s}' integrates to {:.02f}"\
+                  .format(kernel, integral))
         else:
             print(terminalcolors.FAIL, end='')
-            print("    TEST FAILED: 2D kernel '{:s}' integrates to {:.02f}.".format(kernel, integral))
+            print("    TEST FAILED: 2D kernel '{:s}' integrates to {:.02f}."\
+                  .format(kernel, integral))
             print(terminalcolors.RESET, end='')
-    
+
+def test_3d_euclidean(cuda=False)
     # Test kernel normalization in three dimensional euclidean space
     print("Testing kernel normalization in 3D Euclidean space")
     training_points_3d = numpy.zeros(3)[numpy.newaxis,:]
@@ -168,16 +193,32 @@ def main():
     
         def f(x, y, z):
             x = numpy.array((x,y,z))[numpy.newaxis, :]
-            return kde.evaluate.estimate_pdf_brute(x, training_points_3d, kernel=kernel)[0]
+            return kde.evaluate.estimate_pdf_brute(x, training_points_3d, 
+                                                   kernel=kernel, cuda=cuda)[0]
     
         r = (xmin, xmax)
         integral = scipy.integrate.nquad(f, [r for i in range(3)], opts={'epsabs': .1})[0]
         if numpy.isclose(integral, 1, atol=0.1):
-            print("    Test passed: 3D kernel '{:s}' integrates to {:.02f}".format(kernel, integral))
+            print("    Test passed: 3D kernel '{:s}' integrates to {:.02f}"\
+                  .format(kernel, integral))
         else:
             print(terminalcolors.FAIL, end='')
-            print("    TEST FAILED: 3D kernel '{:s}' integrates to {:.02f}.".format(kernel, integral))
+            print("    TEST FAILED: 3D kernel '{:s}' integrates to {:.02f}."\
+                  .format(kernel, integral))
             print(terminalcolors.RESET, end='')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-cuda', dest=cuda, action='store_true',
+                        help="Run tests using cuda backend.")
+    args = parser.parse_arguments()
+    test_1d_euclidean(cuda=args.cuda)
+    test_1d_euclidean_bw2(cuda=args.cuda)
+    test_1d_euclidean_multiple_training(cuda=args.cuda)
+    test_1d_euclidean_ntorus(cuda=args.cuda)
+    test_2d_euclidean(cuda=args.cuda)
+    test_2d_euclidean_ntorus(cuda=args.cuda)
+    test_3d_euclidean(cuda=args.cuda)
 
 if __name__ == "__main__":
     main()
